0c4e507b7b3c9c0f39263efad92ec00a7579b00b
Timmy Li <tyli@cs.uchicago.edu>
2019-11-01 15:02:23 -0500

    plots

--- complexity_analysis/plotter.py
+++ complexity_analysis/plotter.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df = pd.read_csv('TA1Agamma_plots.csv')
+df = pd.read_csv('CRIMEgamma_plots.csv')
 
 
 sns.axes_style("white")
@@ -21,7 +21,7 @@ ax=sns.jointplot(x='gamma',y='ct',data=df2,kind='kde',xlim=[0,0.03])
 plt.show()
 '''
 
-'''
+
 ax = sns.jointplot(y="numSrc", x="srcmean_gamma", data=df,kind='hex')
 plt.title(PREF)
 
@@ -45,7 +45,7 @@ plt.title(PREF)
 
 plt.savefig(PREF +'med_gammahex2.png')
 plt.figure()
-'''
+
 
 PREF = 'CRIME'
 ax = sns.scatterplot(y="numSrc", x="srcmean_gamma", data=df)
